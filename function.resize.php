0e791ebd1bd59ba8dfe0e8cfaaf98488c338044a
Yago FErnandez <yagocaselas@yahoo.es>
2016-04-08 23:34:11 +0200

    Se elimina la función sanitize que ya no se utiliza

--- function.resize.php
+++ function.resize.php
@@ -4,21 +4,15 @@ require 'ImagePath.php';
 require 'Configuration.php';
 require 'Resizer.php';
 
-function sanitize($path) {
-	return urldecode($path);
-}
-
 function resize($imagePath,$opts=null){
 
 	$path = new ImagePath($imagePath);
 	$configuration = new Configuration($opts);
-
 	$resizer = new Resizer($path, $configuration);
-	$newPath = $resizer->doResize();
 
+	$newPath = $resizer->doResize();
 	$cacheFilePath = str_replace($_SERVER['DOCUMENT_ROOT'],'',$newPath);
 
 	return $cacheFilePath;
-	
 }
 
