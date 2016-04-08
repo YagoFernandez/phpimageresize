cf2539262637d5ac8a0a1d96f3cf8bdab5c02532
Yago FErnandez <yagocaselas@yahoo.es>
2016-04-08 23:57:39 +0200

    Extraemos constantes

--- DefaultCommand.php
+++ DefaultCommand.php
@@ -8,16 +8,20 @@
  */
 class DefaultCommand
 {
+    const THUMBNAIL_OPTION = " -thumbnail ";
+    const QUALITY_OPTION = " -quality ";
 
     public function obtainCommand($imagePath, $newPath, $configuration) {
         $opts = $configuration->asHash();
         $w = $configuration->obtainWidth();
         $h = $configuration->obtainHeight();
 
-        $command = $configuration->obtainConvertPath() ." " . escapeshellarg($imagePath) .
-            " -thumbnail ". (!empty($h) ? 'x':'') . $w ."".
-            (isset($opts['maxOnly']) && $opts['maxOnly'] == true ? "\>" : "") .
-            " -quality ". escapeshellarg($opts['quality']) ." ". escapeshellarg($newPath);
+        $command = $configuration->obtainConvertPath()
+            ." " . escapeshellarg($imagePath)
+            .SELF::THUMBNAIL_OPTION. (!empty($h) ? 'x':'')
+            . $w ."". (isset($opts['maxOnly']) && $opts['maxOnly'] == true ? "\>" : "")
+            .SELF::QUALITY_OPTION. escapeshellarg($opts['quality'])
+            ." ". escapeshellarg($newPath);
 
         return $command;
     }
