0a828958a42abe3e39223abf8b67c12403650cef
Yago FErnandez <yagocaselas@yahoo.es>
2016-04-08 23:52:42 +0200

    Se extraen constantes

--- CropCommand.php
+++ CropCommand.php
@@ -8,19 +8,29 @@
  */
 class CropCommand
 {
+    const RESIZE_OPTION = " -resize ";
+    const SIZE_OPTION = " -size ";
+    const CANVAS_COLOR_OPTION = " xc:";
+    const QUALITY_OPTION = " +swap -gravity center -composite -quality ";
+
     public function obtainCommand($imagePath, $newPath, $configuration) {
         $opts = $configuration->asHash();
         $w = $configuration->obtainWidth();
         $h = $configuration->obtainHeight();
         $resize = $this->composeResizeOptions($imagePath, $configuration);
 
-        $cmd = $configuration->obtainConvertPath() ." ". escapeshellarg($imagePath) ." -resize ". escapeshellarg($resize) .
-            " -size ". escapeshellarg($w ."x". $h) .
-            " xc:". escapeshellarg($opts['canvas-color']) .
-            " +swap -gravity center -composite -quality ". escapeshellarg($opts['quality'])." ".escapeshellarg($newPath);
+        $cmd = $configuration->obtainConvertPath()
+            ." ". escapeshellarg($imagePath)
+            .self::RESIZE_OPTION. escapeshellarg($resize)
+            .self::SIZE_OPTION. escapeshellarg($w ."x". $h)
+            .self::CANVAS_COLOR_OPTION. escapeshellarg($opts['canvas-color'])
+            .self::QUALITY_OPTION. escapeshellarg($opts['quality'])
+            ." ".escapeshellarg($newPath);
 
         return $cmd;
     }
+
+
 
     public function isPanoramic($imagePath) {
         list($width,$height) = getimagesize($imagePath);
@@ -34,7 +44,7 @@ class CropCommand
 
         $resize = "x".$h;
 
-        $hasCrop = (true === $opts['crop']);
+        $hasCrop = (true === $configuration->obtainCrop());
 
         if(!$hasCrop && $this->isPanoramic($imagePath)):
             $resize = $w;
