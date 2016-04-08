8a43ca1d2cc2bb03ca9458d1cd1c49e863c88775
Yago FErnandez <yagocaselas@yahoo.es>
2016-04-09 00:04:14 +0200

    Se crean funciones para las opciones Thumbnail y Width

--- DefaultCommand.php
+++ DefaultCommand.php
@@ -15,14 +15,36 @@ class DefaultCommand
         $opts = $configuration->asHash();
         $w = $configuration->obtainWidth();
         $h = $configuration->obtainHeight();
+        $maxOnly = $opts['maxOnly'];
+        $quality = $opts['quality'];
 
         $command = $configuration->obtainConvertPath()
             ." " . escapeshellarg($imagePath)
-            .SELF::THUMBNAIL_OPTION. (!empty($h) ? 'x':'')
-            . $w ."". (isset($opts['maxOnly']) && $opts['maxOnly'] == true ? "\>" : "")
-            .SELF::QUALITY_OPTION. escapeshellarg($opts['quality'])
+            .SELF::THUMBNAIL_OPTION. $this->obtainThumbnailOption($h)
+            . $w ."". $this->obtainWidthOption($maxOnly)
+            .SELF::QUALITY_OPTION. escapeshellarg($quality)
             ." ". escapeshellarg($newPath);
 
         return $command;
+    }
+
+    function obtainThumbnailOption($height) {
+
+        $result = '';
+
+        if (!empty($height))
+            $result = 'x';
+
+        return $result;
+    }
+
+    function obtainWidthOption($maxOnly) {
+
+        $result = "";
+
+        if (isset($maxOnly) && $maxOnly == true)
+            $result = "\>";
+
+        return $result;
     }
 }
